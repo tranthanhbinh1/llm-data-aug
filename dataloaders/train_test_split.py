dd7d54d3602c000fa4c8e530b0746bd274c84fd2
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-03-26 06:16:35 +0700

    custom train test splitter

--- dataloaders/train_test_split.py
+++ dataloaders/train_test_split.py
@@ -25,6 +25,12 @@ class DataScenario(StrEnum):
         "downsampled",
     )
 
+    LLM_GENERATED = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        "data",
+        "llm_generated",
+    )
+
 
 class DatasetType(StrEnum):
     """
@@ -44,18 +50,21 @@ class TrainTestSplit:
     @staticmethod
     def load_data(data_scenario: DataScenario):
         # Have a name for the file
-        file_name = data_scenario.value + data_scenario.name.lower() + ".csv"
-        return pd.read_csv(file_name)
+        file_name = data_scenario.name.lower() + "_user_reviews.csv"
+        return pd.read_csv(os.path.join(data_scenario.value, file_name))
 
     @staticmethod
     def custom_train_test_split(
-        df: pd.DataFrame, test_size: float = 0.2, random_state: int = 42
-    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        texts: list[str],
+        labels: list[str],
+        test_size: float = 0.2,
+        random_state: int = 42,
+    ):
         # Split the data into training and testing sets
-        train_df, test_df = train_test_split(
-            df, test_size=test_size, random_state=random_state
+        train_texts, test_texts, train_labels, test_labels = train_test_split(
+            texts, labels, test_size=test_size, random_state=random_state
         )
-        return train_df, test_df
+        return train_texts, test_texts, train_labels, test_labels
 
     @staticmethod
     def save_data(
@@ -63,21 +72,42 @@ class TrainTestSplit:
         data_scenario: DataScenario,
         dataset_type: DatasetType,
     ):
-        file_name = (
-            data_scenario.value
-            + f"{dataset_type}"
-            + data_scenario.name.lower()
-            + ".csv"
+        file_name = os.path.join(
+            data_scenario.value,
+            f"{dataset_type}_{data_scenario.name.lower()}_user_reviews.csv",
         )
-        df.to_csv(file_name, index=False)
+        return df.to_csv(file_name, index=False)
 
     @staticmethod
     def run_train_test_split(
-        data_scenario: DataScenario, test_size: float = 0.2, random_state: int = 42
+        data_scenario: DataScenario,
+        test_size: float = 0.2,
+        random_state: int = 42,
+        texts_column: str = "emoji to text",  # NOTE: This column has corporated emoji meanings and text
+        labels_column: str = "Sentiment",
     ):
         df = TrainTestSplit.load_data(data_scenario)
-        train_df, test_df = TrainTestSplit.custom_train_test_split(
-            df, test_size=test_size, random_state=random_state
+
+        # Split the data into training and testing sets
+        train_texts, test_texts, train_labels, test_labels = (
+            TrainTestSplit.custom_train_test_split(
+                df[texts_column].tolist(),
+                df[labels_column].tolist(),
+                test_size=test_size,
+                random_state=random_state,
+            )
         )
-        TrainTestSplit.save_data(train_df, data_scenario, DatasetType.TRAIN)
-        TrainTestSplit.save_data(test_df, data_scenario, DatasetType.TEST)
+
+        # Split the training data into training and validation sets
+        train_texts, val_texts, train_labels, val_labels = train_test_split(
+            train_texts, train_labels, test_size=test_size, random_state=random_state
+        )
+
+        train_data = pd.DataFrame({"text": train_texts, "label": train_labels})
+        val_data = pd.DataFrame({"text": val_texts, "label": val_labels})
+        test_data = pd.DataFrame({"text": test_texts, "label": test_labels})
+
+        # Save the data
+        TrainTestSplit.save_data(train_data, data_scenario, DatasetType.TRAIN)
+        TrainTestSplit.save_data(val_data, data_scenario, DatasetType.VAL)
+        TrainTestSplit.save_data(test_data, data_scenario, DatasetType.TEST)
