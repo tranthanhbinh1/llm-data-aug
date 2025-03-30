985d353fe2d6064c4f3b3ed093622a2c4f82bb88
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-03-30 19:28:57 +0700

    Add neutral user reviews dataset and update data generation logic to include sentiment prompts

--- synthesizer/generator.py
+++ synthesizer/generator.py
@@ -1,7 +1,9 @@
 import time
 import instructor
-import google.generativeai as genai
-from .models import UserReview
+from instructor import Instructor, AsyncInstructor
+from google import genai
+from openai import OpenAI
+from .models import SentimentPrompt, UserReviews
 from dotenv import load_dotenv
 import os
 import pandas as pd
@@ -15,16 +17,15 @@ class DataGenerator:
     SENTIMENT_MAPPING = {"positive": 1, "neutral": 2, "negative": 0}
     PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_PATH = os.path.join(PROJECT_ROOT, "data")
-    RATE_LIMIT = 15
+    MAX_RETRIES = 3
 
-    def __init__(self):
-        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    def __init__(self, instructor: AsyncInstructor):
+        self.instructor = instructor
 
-        self.client = instructor.from_gemini(
-            client=genai.GenerativeModel(
-                model_name="models/gemini-2.0-flash",
-            ),
-            mode=instructor.Mode.GEMINI_JSON,
+    def get_client(self):
+        client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+        self.client: AsyncInstructor = instructor.from_genai(
+            client, mode=instructor.Mode.GENAI_TOOLS
         )
 
     @classmethod
@@ -52,12 +53,15 @@ class DataGenerator:
         sentiment: Literal["positive", "neutral", "negative"],
         num_examples: int = 5,
     ) -> list[dict[str, str]]:
+        """Get examples from the original dataset for each LLM call"""
+        label_mapping = {"Positive": 1, "Neutral": 2, "Negative": 0}
+
+        data = pd.read_csv("data/cleaned_user_reviews.csv")
+        data["Sentiment"] = data["Sentiment"].map(label_mapping)
         review_examples = (
-            pd.read_csv("data/class_samples/user_review_class_samples.csv")[
-                ["Review", "Sentiment"]
-            ]
+            data[["Review", "Sentiment"]]
             .query(f"Sentiment == {cls.SENTIMENT_MAPPING[sentiment]}")
-            .head(num_examples)
+            .sample(n=num_examples)
             .to_dict(orient="records")
         )
 
@@ -71,76 +75,117 @@ class DataGenerator:
 
         return few_shot_examples
 
-    def generate_reviews(self, examples: list[dict[str, str]]) -> UserReview:
-        resp = self.client.messages.create(
+    def generate_reviews(
+        self,
+        examples: list[dict[str, str]],
+        prompt: SentimentPrompt,
+        model: str = "gemini-2.0-flash",
+        batch_size: int = 15,
+    ) -> UserReviews:
+        resp = self.instructor.messages.create(
             messages=[
                 {
                     "role": "system",
                     "content": """You are a helpful assistant that generates user reviews for a product or service.
                         You are proficient in Vietnamese, and will be asked to generate reviews in Vietnamese.
-                        Please do not include trailing characters.
                         """,
                 },
+                # TODO: change the prompt once we want to generate Neutral, Positive reviews
                 {
                     "role": "user",
-                    "content": "Hãy tạo ra các bình luận tương tự nói về trải nghiệm: Trong vai là một khách hàng vừa trải qua một một trải nghiệm tồi tệ, ko hài lòng về chất lượng dịch vụ của McDonald. Hãy tạo, generate ra các bình luận tương tự nói về trải nghiệm đó.",
+                    "content": prompt.value.format(batch_size=batch_size),
                 },
                 *examples,
             ],
-            response_model=UserReview,
+            model=model,
+            max_retries=self.MAX_RETRIES,
+            response_model=UserReviews,
         )
 
-        return cast(UserReview, resp)
+        return cast(UserReviews, resp)
 
     def generate_reviews_batch(
         self,
         examples: list[dict[str, str]],
         sentiment: Literal["positive", "neutral", "negative"],
+        prompt: SentimentPrompt,
+        model: str = "gemini-2.0-flash",
         num_examples: int = 5,
-    ) -> list[UserReview]:
-        """We are being rate-limited to 15 requests per minute, so we need to generate reviews in batches."""
-        examples = self.generate_examples(
-            num_examples=num_examples, sentiment=sentiment
-        )
-        target_number = self.get_target_number(sentiment)
-        logger.info(f"Target number: {target_number}")
-        reviews = []
+        batch_size: int = 15,
+    ) -> list[UserReviews]:
+        target_batches = self.get_target_number(sentiment) // batch_size
+        logger.info(f"Target batches: {target_batches}")
+
+        batched_records: list[UserReviews] = []
         _hit_count = 0
-        while len(reviews) < target_number:
+        while len(batched_records) < target_batches:
             try:
-                reviews.extend(
-                    [self.generate_reviews(examples) for _ in range(num_examples)]
+                examples = self.generate_examples(
+                    num_examples=num_examples, sentiment=sentiment
                 )
-                _hit_count += 1
-                logger.info(f"Generated {len(reviews)} reviews")
-                logger.info(f"Hit count: {_hit_count}")
-                logger.info(f"Generated reviews: {reviews[-1]}")
+                logger.info(f"Generating reviews for batch {len(batched_records) + 1}")
+                generated_samples = self.generate_reviews(
+                    examples, prompt, model, batch_size
+                )
+                logger.info(f"Generated reviews: {generated_samples}")
+                batched_records.append(generated_samples)
+                _hit_count += self.MAX_RETRIES
+                logger.info(f"Hit count (including retries): {_hit_count}")
             except Exception as e:
+                if "429" in str(e):
+                    logger.error("Rate limit reached, sleeping for 60 seconds")
+                    time.sleep(60)
+                    continue
                 logger.error(f"Error generating reviews: {e}")
                 continue
-            if _hit_count >= self.RATE_LIMIT:
-                logger.info("Rate limit reached, sleeping for 60 seconds")
-                time.sleep(60)
-                _hit_count = 0
-        return reviews
+            # Test
+            break
+        return batched_records
 
-    def save_reviews(self, reviews: list[UserReview], path: str):
-        pd.DataFrame(reviews).to_csv(path, index=False)
+    def save_reviews(self, batched_records: list[UserReviews], path: str):
+        batched_df = []
+        for rec in batched_records:
+            _df = pd.DataFrame(rec.model_dump())
+            batched_df.append(_df)
+        batched_df = pd.concat(batched_df)
+        batched_df.to_csv(path, index=False)
 
 
 if __name__ == "__main__":
+    sentiment_prompt_mapping = {
+        "negative": SentimentPrompt.NEGATIVE,
+        "neutral": SentimentPrompt.NEUTRAL,
+        "positive": SentimentPrompt.POSITIVE,
+    }
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--sentiment", type=str, default="negative")
+    parser.add_argument("--num_examples", type=int, default=5)
+    parser.add_argument("--model", type=str, default="gemini-2.0-flash")
     args = parser.parse_args()
 
-    generator = DataGenerator()
+    # client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    # instance = instructor.from_genai(client, mode=instructor.Mode.GENAI_TOOLS)
+
+    instance = instructor.from_openai(
+        OpenAI(
+            base_url="http://localhost:11434/v1",
+            api_key="ollama",  # required, but unused
+        ),
+        mode=instructor.Mode.JSON,
+    )
+
+    generator = DataGenerator(instance)
     examples: list[dict[str, str]] = generator.generate_examples(
         num_examples=5, sentiment=args.sentiment
     )
-    reviews: list[UserReview] = generator.generate_reviews_batch(
-        examples=examples, sentiment=args.sentiment, num_examples=5
+    reviews: list[UserReviews] = generator.generate_reviews_batch(
+        examples=examples,
+        sentiment=args.sentiment,
+        num_examples=args.num_examples,
+        prompt=sentiment_prompt_mapping[args.sentiment],
+        model=args.model,
     )
     generator.save_reviews(
         reviews, f"data/llm_generated/{args.sentiment}_user_reviews.csv"
--- synthesizer/models.py
+++ synthesizer/models.py
@@ -1,13 +1,21 @@
 from pydantic import BaseModel, Field
+from enum import StrEnum
 
 
 class UserReview(BaseModel):
-    reviews: list[str] = Field(description="The user reviews", alias="Review")
-    sentiments: list[int] = Field(
+    review: str = Field(description="The user reviews")
+    sentiment: int = Field(
         description="The sentiments of the reviews, 0 for negative, 1 for positive, 2 for neutral",
-        alias="Sentiment",
     )
-    n: int = Field(
-        description="The number of reviews to generate",
-        default=10,
+
+
+class UserReviews(BaseModel):
+    reviews: list[UserReview] = Field(
+        description="The user reviews", min_length=15, max_length=15
     )
+
+
+class SentimentPrompt(StrEnum):
+    NEGATIVE = "Hãy tạo ra {batch_size} bình luận tương tự nói về trải nghiệm: Trong vai là một khách hàng vừa trải qua một một trải nghiệm tồi tệ, ko hài lòng về chất lượng dịch vụ của McDonald."
+    POSITIVE = "Hãy tạo ra {batch_size} bình luận tương tự nói về trải nghiệm: Trong vai là một khách hàng vừa trải qua một một trải nghiệm tốt, hài lòng về chất lượng dịch vụ của McDonald."
+    NEUTRAL = "Hãy tạo ra {batch_size} bình luận tương tự nói về trải nghiệm: Trong vai là một khách hàng vừa trải qua một một trải nghiệm trung bình, không tốt cũng không xấu về chất lượng dịch vụ của McDonald."
