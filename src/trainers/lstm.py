08c29b2554f38998b57e82a8638ddb5a97d17150
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-05-05 08:18:48 +0700

    init LSTM code

--- src/trainers/lstm.py
+++ src/trainers/lstm.py
@@ -1,1 +1,26 @@
-# TODO: implement LSTM trainer
+import time
+import pandas as pd
+from sklearn.metrics import classification_report, f1_score, accuracy_score
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from loguru import logger as logging
+import numpy as np
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, AutoModel
+from sklearn.preprocessing import LabelEncoder
+from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
+from tqdm import tqdm
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader, RandomSampler, TensorDataset
+
+
+class LSTM(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, output_dim, dropout):
+        super().__init__()
+        self.embedding = nn.Embedding(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
