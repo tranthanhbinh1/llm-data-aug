2827726f6ed5ae5d40e9daf691e7f60aa86bae66
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2025-06-28 22:31:53 +0700

    update pipeline

--- src/worker/jobs/full_pipeline.py
+++ src/worker/jobs/full_pipeline.py
@@ -3,6 +3,8 @@ import dagster as dg
 from src.worker.assets.prompt import prompt_asset
 from src.worker.assets.synthetic_data import synthetic_data_asset
 from src.worker.assets.score import score_asset
+from src.worker.assets.preprocessed_data import preprocessed_data_asset
+from src.worker.assets.multi_trainer_scores import multi_trainer_scores_asset
 
 
 # Create the full pipeline job
@@ -13,6 +15,8 @@ full_pipeline_job = dg.define_asset_job(
         prompt_asset,
         synthetic_data_asset,
         score_asset,
+        preprocessed_data_asset,
+        multi_trainer_scores_asset,
     ),
 )
 
